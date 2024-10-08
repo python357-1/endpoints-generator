0240af91894344098a132c018035ec54ea3a33d9
python357-1 <jb2101554@gmail.com>
2024-10-08 02:18:01 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace endpointgenerator;
+
+internal class Program {
+	static void Main(string[] args) {
+		if (args.Length == 0) {
+			Console.WriteLine("no");
+			return;
+		}
+
+		if (args.Length == 1) {
+			var runtimeAssemblies = Directory.GetFiles(RuntimeEnvironment.GetRuntimeDirectory(), "*.dll");
+			var paths = new List<string>(runtimeAssemblies);
+			paths.Add(Path.Join(Directory.GetCurrentDirectory(), args[0]));
+			paths.Add("Microsoft.AspNetCore");
+			var resolver = new PathAssemblyResolver(paths);
+
+			var mlc = new MetadataLoadContext(resolver);
+
+			using (mlc) {
+				var assm = mlc.LoadFromAssemblyPath(Path.GetFileName(args[0]));
+				var types = assm.GetTypes();
+				foreach (var controller in types.Where(x =>
+					         x.Name.Contains("controller", StringComparison.InvariantCultureIgnoreCase))) {
+					Console.WriteLine(controller.Name);
+				}
+
+			}
+			
+		}
+	}
+}
---
+++ install.sh
@@ -0,0 +1,5 @@
+dotnet build
+
+dotnet pack
+
+dotnet tool update --global --add-source ~/projects/dotnet-endpoint-generator/endpointgenerator/nupkg endpointgenerator
